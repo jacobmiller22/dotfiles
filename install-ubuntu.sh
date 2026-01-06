b88733eae83c94b341314a5fee742b8a05eae40b
Jacob <jacobmiller22@603e5f680b93.lan>
2026-01-06 17:47:07 -0500

    ubuntu

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,4 +1,23 @@
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Assumes the following is installed
+# - git
+# - curl
+# - stow
 
-sh install-uni.sh
+# (Re)Install Neovim
+curr=$(pwd)
+suffix=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 5) # Generate 5 random chars
+echo "suffix is $suffix"
+stagedir=/tmp/nvim-install-$suffix
+mkdir $stagedir && cd $stagedir
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+cd $curr
+rm -rf $stagedir
+
+
+# Setup zsh
+stow zsh
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s $(which zsh) $USER
+source ~/.zshrc
